4b52f633d2f51c9395641a3995e79df59d6a9fc9
florindahasanii <93677978+florindahasanii@users.noreply.github.com>
2022-09-01 16:10:13 +0200

    Create mars.go

---
+++ Operators/mars.go
@@ -0,0 +1,10 @@
+ package main
+
+ import "fmt"
+
+ func main(){
+   fmt.Print("My weight on the surface of Mars is: ")
+   fmt.Print(50*0.3783)
+   fmt.Print(".1kg in Mars would be: ")
+   fmt.Print(1*0.3783,".")
+ }   
